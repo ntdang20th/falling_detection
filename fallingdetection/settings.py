ef0da698ccc983d0f6fae1e91a8a870db7b48bf1
Dang <ntdang_20th@student.agu.edu.vn>
2022-11-13 12:02:56 +0700

    collect static

--- fallingdetection/settings.py
+++ fallingdetection/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = 'staticfiles'
 REST_FRAMEWORK = {
     # 'DEFAULT_RENDERER_CLASSES': ('rest_framework.renderers.JSONRenderer', ),
 
